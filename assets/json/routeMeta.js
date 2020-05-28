99d3fd57e161f99e4473d323943d005f4bf3290f
zhangqiang <315078932@qq.com>
2020-05-28 16:48:10 +0800

    路由json修改

---
+++ assets/json/routeMeta.js
@@ -0,0 +1,97 @@
+export default
+{
+    login: {
+        meta: { title: '登录' },
+        sole: 100,
+        hidden: true
+    },
+    register: {
+        meta: { title: '注册' },
+        sole: 100,
+        hidden: true
+    },
+    quicklyFind: {
+        meta: { title: '快速找' },
+        sole: 1
+    },
+    'quicklyFind-team': {
+        hidden: true,
+        sole: 101,
+        meta: { title: '找团队' }
+    },
+    'quicklyFind-company': {
+        hidden: true,
+        sole: 102,
+        meta: { title: '找企业' }
+    },
+    'quicklyFind-performance': {
+        hidden: true,
+        sole: 103,
+        meta: { title: '找业绩' }
+    },
+    'quicklyFind-report': {
+        hidden: true,
+        sole: 104,
+        meta: { title: '企业报告查询' }
+    },
+    'quicklyFind-aptitude': {
+        hidden: true,
+        sole: 105,
+        meta: { title: '人员/资质查询' }
+    },
+    joinCompany: {
+        meta: { title: '需要加盟公司' },
+        sole: 2,
+        hidden: true
+    },
+    workTeam: {
+        meta: { title: '需要干活团队' },
+        sole: 3,
+        hidden: true
+    },
+    coopCompany: {
+        meta: { title: '需要合作公司' },
+        sole: 4,
+        hidden: true
+    },
+    entrust: {
+        meta: { title: '一站委托' },
+        sole: 5,
+        hidden: true
+    },
+    tenderSquare: {
+        meta: { title: '招投标广场' },
+        sole: 6
+    },
+    'tenderSquare-tenderNotice': {
+        meta: { title: '招标公告' },
+        sole: 601
+    },
+    'tenderSquare-changeNotice': {
+        meta: { title: '变更公告' },
+        sole: 602
+    },
+    'tenderSquare-bidNotice': {
+        meta: { title: '中标公告' },
+        sole: 603
+    },
+    'tenderSquare-info': {
+        meta: { title: '详情' },
+        hidden: true
+    },
+    underProject: {
+        meta: { title: '在建项目' },
+        sole: 7,
+        hidden: true
+    },
+    laws: {
+        meta: { title: '政策法规' },
+        sole: 8,
+        hidden: true
+    },
+    notfound: {
+        sole: 999,
+        meta: { title: '404' },
+        hidden: true
+    }
+}
